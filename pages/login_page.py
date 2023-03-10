32d00dd7cb7943567f9b1027144cabe67e81264b
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-10 20:01:45 +0300

    added tests for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,12 +10,14 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', \
+            'Current URL should be http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), f'There is no login form on {self.url} page.'
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_FORM), f'There is no registration form on {self.url} page.'
---
+++ test_login_page.py
@@ -0,0 +1,24 @@
+from .pages.login_page import LoginPage
+
+
+def test_should_be_correct_url(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_should_be_login_form_on_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_should_be_registartion_form_on_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+# pytest -v --tb=line --language=en test_login_page.py
