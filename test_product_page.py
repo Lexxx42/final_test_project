cd8c05b1b893f80eecaf3144455edd455d1e5651
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-12 16:15:04 +0300

    check autotests with new promo link

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,8 @@ import time
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
