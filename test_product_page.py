488be165e780cb09f8604e7f0a972bac1df1a519
Alexandr Konukhov <jesusmisha@gmail.com>
2023-03-16 10:45:13 +0300

    added mark need_review

--- test_product_page.py
+++ test_product_page.py
@@ -15,6 +15,7 @@ from .pages.login_page import LoginPage
 LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
+@pytest.mark.need_review
 @pytest.mark.params
 @pytest.mark.parametrize('link', ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0',
                                   'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1',
@@ -72,6 +73,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 @pytest.mark.adv_inheritance
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
@@ -81,6 +83,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 @pytest.mark.basket
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
@@ -113,6 +116,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
         page = ProductPage(browser, link)
