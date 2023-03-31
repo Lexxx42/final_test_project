118795b7f638e3b4465fb2981d24ea887e0dd1fa
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-31 20:32:24 +0300

    fixed log path

--- logger.py
+++ logger.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
     handlers=[
-        logging.FileHandler(filename='../logs/tests.log', encoding='utf-8', mode='w'),
+        logging.FileHandler(filename='./logs/tests.log', encoding='utf-8', mode='w'),
         logging.StreamHandler()],
     level=logging.INFO,
 )
