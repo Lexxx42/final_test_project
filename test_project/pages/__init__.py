c3092647cba5857e7bbe72903a1ada325df56698
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-01 08:25:28 +0300

    forgot to add MainPage

--- test_project/pages/__init__.py
+++ test_project/pages/__init__.py
@@ -2,5 +2,6 @@ from .base_page import BasePage
 from .login_page import LoginPage
 from .product_page import ProductPage
 from .basket_page import BasketPage
+from .main_page import MainPage
 
-__all__ = ['BasePage', 'LoginPage', 'ProductPage', 'BasketPage']
+__all__ = ['BasePage', 'LoginPage', 'ProductPage', 'BasketPage', 'MainPage']
